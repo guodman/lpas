db9688adde448f396f784153e0a32aaaec15e4bb
Doug Reeves <guodman@gmail.com>
2008-07-02 23:08:43 +0000

    added license header
    git-svn-id: svn+ssh://guodman.org/usr/local/svn/misc/trunk/food@889 3783ef2b-3708-4680-8892-867c7557413e

--- preferences.php
+++ preferences.php
@@ -1,3 +1,24 @@
+<!--
+/**
+ * Copyright (C) 2007 Doug Reeves dreeves@bluebottle.com
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * see http://www.gnu.org/licenses/gpl.html for more information
+ */
+-->
 <?php
 session_start();
 mysql_connect("localhost", "doug");
